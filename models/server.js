f2ceb6390baa2283b788e4a707594fdf76bff698
Leonardo Cano <leomaucano111@gmail.com>
2021-06-04 21:06:54 -0300

    First Commit

---
+++ models/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+// servidor de express
+const http = require('http');
+// configuracion del socket server
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // http server
+        this.server = http.createServer(this.app)
+        // consfiguraciones de sockets
+        this.io = socketio(this.server)
+        this.configurarSockets()
+    }
+
+    middlewares() {
+        this.app.use(express.static(path.resolve(__dirname, '../public')))
+    }
+
+    configurarSockets() {
+        new Sockets(this.io)
+    }
+
+    execute() {
+        this.middlewares()
+        this.server.listen(this.port, () => {
+            console.log("server corriendo " + this.port);
+        });
+    }
+}
+
+module.exports = Server
